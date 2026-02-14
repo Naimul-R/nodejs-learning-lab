b84927fcbc2adf7fb8d59f8960478d37406baf9e
Naimul-R <naimul.rabby08@gmail.com>
2026-02-14 12:52:03 +0600

    learn variable how works in JS

--- 01-Node-Basics/first.js
+++ 01-Node-Basics/first.js
@@ -1,3 +1,15 @@
 console.log("This is my first JS 🤩");
 console.log("Niamul Islam Rabby.");
 console.log("I love JavaScript!");
+
+// Variables
+fullName = "Iron Man";
+age = 25;
+price = 100.00;
+//Null value
+x = null;
+//Undefine value
+y = undefined
+
+console.log(fullName);
+console.log(x);
---
+++ 01-Node-Basics/variables.js
@@ -0,0 +1,26 @@
+// Variable's Rules 
+/* 1 -Variable's name are case sensitive;
+   2 -Only letters, digits, underscore(_) and $ is allowed. (Not even space);
+   3 -Only Letter, underscore(_) and $ should be in 1st charecter;
+   4 -Reversed word cannot be variable
+*/
+
+// Case sensetive
+ProgramName = "JavaScript"
+PROGRAMNAME = "Java"
+
+console.log(ProgramName)
+console.log(PROGRAMNAME)
+console.log()
+
+// 2,3 letters, digits, underscore and $
+console.log("Rules to create variable -->")
+nick_name = "Naimul"
+fullName = "Naimul Islam Rabby"
+$Age = 25
+price$val = 100.0
+
+console.log(nick_name)
+console.log(fullName)
+console.log($Age)
+console.log(price$val)
