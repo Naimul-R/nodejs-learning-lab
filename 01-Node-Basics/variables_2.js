ecd78de91a26e95ea84ea84fb7e137f5e8edbfd4
Naimul-R <naimul.rabby08@gmail.com>
2026-02-15 16:13:17 +0600

    learn variable type like let var and const

---
+++ 01-Node-Basics/variables_2.js
@@ -0,0 +1,12 @@
+/* let, const & var
+var : variable can be re-declared and updated. A global scope variable (Old version) before 2015.
+let : variable cannot be re-declared but can be updated. A block scope variable
+const : variable cannot be re-declared or updated. A block scope variable
+*/
+// Let variable
+let fullName = "Naimul Rabby"
+let age = 25
+let totalPrice = 100.00
+
+console.log(fullName)
+console.log(age)
