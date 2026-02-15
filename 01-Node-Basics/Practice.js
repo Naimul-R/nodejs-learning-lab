1e4f7197129dd7e0fe2ff5e141dbe15e0dcfa1e0
Naimul-R <naimul.rabby08@gmail.com>
2026-02-15 23:58:09 +0600

    practice Q 1

---
+++ 01-Node-Basics/Practice.js
@@ -0,0 +1,10 @@
+// Q - 1
+const product = {
+    Pdt_name : "Parker Jotter Standard CT ball pen (Black).",
+    ratting : 7002,
+    ad : "Deal of the day",
+    price : 270,
+    offer : 10
+};
+
+console.log(product);
