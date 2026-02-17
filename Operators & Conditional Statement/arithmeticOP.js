596c9cca42bef44cfda6d9350aa8c6c6887ceb71
Naimul-R <naimul.rabby08@gmail.com>
2026-02-17 23:19:44 +0600

    Practice different type of operators.

--- Operators & Conditional Statement/arithmeticOP.js
+++ Operators & Conditional Statement/arithmeticOP.js
@@ -1,6 +1,20 @@
 // Arithmetic Operators
 
-let a = 5;
-let b = 10;
+let a = 10;
+let b = 5;
 
+console.log("a =", a, "& b =", b);
 console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+
+// Modulus --> remainder.
+console.log("a % b =", a % b);
+
+// Exponentiation --> symbol (**) two estores
+console.log("a ** b", a ** b);
+
+/* Unary operators
+    Two Types of operators 1- Increment (++), 2- Decrement (--).
+*/
