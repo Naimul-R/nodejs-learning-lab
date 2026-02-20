35d6c07d08bd21652ba2caa2f2a2d5ea1525919b
Naimul-R <naimul.rabby08@gmail.com>
2026-02-20 23:33:36 +0600

    Arithmetic Operators notes are done.

--- Operators & Conditional Statement/arithmeticOP.js
+++ Operators & Conditional Statement/arithmeticOP.js
@@ -18,3 +18,24 @@ console.log("a ** b", a ** b);
 /* Unary operators
     Two Types of operators 1- Increment (++), 2- Decrement (--).
 */
+// Increment -->
+let x = 10;
+let y = 5;
+
+console.log("a =", a, "& b =", b);
+x ++ // Increment 11
+console.log("x =", x);
+
+// Decrement -->
+y --; // Decrement
+console.log("y =",y);
+
+/* Two type of increment and decrement -->
+    1- post-increment (a++) 2- pre-increment (++a)
+    1- post-decrement (a--) 2- pre-decrement (--a)
+*/
+// Pre-Increment
+let i = 15
+let j = 20
+console.log("++i =", ++i);
+console.log("--j =", --j);
