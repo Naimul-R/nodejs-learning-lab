51a1b0123f3de41ebc94341c7d2a9a79576de1e2
Naimul-R <naimul.rabby08@gmail.com>
2026-02-23 20:57:08 +0600

    Learn all Assignment Operator.

---
+++ Operators & Conditional Statement/AssignmentOP.js
@@ -0,0 +1,25 @@
+// Assignment Operators are --> +, +=, -=,
+
+let a = 5;
+let b = 10;
+
+a += 5;
+b -= 5;
+
+console.log("a =", a, "& b =", b);
+// Plus 
+console.log("a += ", a); // 10
+// Substraction 
+console.log("b -= ", b); // 5
+// Multiplay 
+a *= 5;
+console.log("a *= ", a); // 50
+// Devided 
+b /= 5;
+console.log("b /= ", b); // 1
+// Modulu
+b %= 2;
+console.log("b %= ", b); // 1
+// Exponential
+a **= 2;
+console.log("a **= ", a); // 2500
