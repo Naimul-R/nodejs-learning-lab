ad712f4dcd3e8cd74dfe7fbdec9e8ee410b30c45
Naimul-R <naimul.rabby08@gmail.com>
2026-02-27 22:05:41 +0600

    Finally, study conditional statement is now done.

--- Operators & Conditional Statement/conditional_if.js
+++ Operators & Conditional Statement/conditional_if.js
@@ -1,5 +1,6 @@
 // To implement some condition in the code
 // syntex --> Rules
+// MDS Doc --> is document theory for all concept for js
 
 // first--> If statement --->
 let age = 16;
@@ -43,3 +44,36 @@ if (num % 2 === 0) {
 } else {
     console.log(num, "Is Odd");
 }
+
+// else-if Statement for multiple condition 
+let modee = "blue";
+let colorr;
+
+if (modee === "dark") {
+    color = "black";
+} else if (modee === "blue") {
+    color = "blue";
+} else if (modee === "pink") {
+    color = "pink";
+} else {
+    color = "white";
+}
+
+console.log(color);
+
+/* Ternary Operator 
+    condition ? expression_if_true : expression_if_false;
+
+    Structure Explanation:
+
+    condition → Boolean expression (evaluates to true or false)
+    ? → Separates condition from the true case
+    expression_if_true → Executes if condition is true
+    : → Separates true case from false case
+    expression_if_false → Executes if condition is false
+*/
+let agee = 25
+
+let result = age <= 18 ? "Adult" : "Not Adult";
+console.log(result);
+
