93e255b57aa697034905b61d94d79517690cd7ae
Naimul-R <naimul.rabby08@gmail.com>
2026-03-12 00:29:43 +0600

    while-loop and do-while loop is completed.

---
+++ Loops/while_loop.js
@@ -0,0 +1,21 @@
+/* While loop-->
+    while (condition) {
+        (do some work)
+    }
+*/
+let name = 1;
+while (name <= 8) {
+    console.log("Naimul Rabby")
+    name++
+}
+
+/* Do-While loop-->
+    do {
+        (do some work)
+    } while (condition); 
+*/
+let i = 1;
+do {
+    console.log("Naimul");
+    i++;
+} while (i <= 5);
