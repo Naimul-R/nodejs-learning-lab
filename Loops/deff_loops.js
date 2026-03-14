2bc508ae542642834bdc6d23631f95db826e77f7
Naimul-R <naimul.rabby08@gmail.com>
2026-03-14 23:06:24 +0600

    Study different loops like for-of and for-in.

--- Loops/deff_loops.js
+++ Loops/deff_loops.js
@@ -3,6 +3,7 @@
         (do some work)
     }
 */
+console.log("===== For-of Loop =====");
 let str = "Naimul Rabby";
 let size = 0;
 
@@ -12,3 +13,21 @@ for (let val of str) { // Iterator -> character
 }
 
 console.log("String size = ", size);
+
+/* for-in loop --> use in object and arry
+    for (let key in objVar) {
+        (do some work)
+    }
+*/
+// For-in loop
+console.log("\n===== For-In Loop =====");
+let student = {
+    name: "Jhony",
+    age: 20,
+    cgpa: 8.67,
+    isPass: true,
+};
+
+for (let key in student) {
+    console.log("Key= ", key , " Value= ", student[key]);
+}
