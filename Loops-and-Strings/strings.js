ce7957fe53f1f76b5f0d2e8a92da1f1e65b861c0
Naimul-R <naimul.rabby08@gmail.com>
2026-03-16 16:17:26 +0600

    study basic string properties and modules

---
+++ Loops-and-Strings/strings.js
@@ -0,0 +1,11 @@
+// String is a sequence of characters used to represent text
+
+// Create strings
+let str = "I love js";
+let str2 = 'Its an amezing language';
+
+// Inbuild properties 
+str.length 
+
+// String indicate ---> str/strName[position]
+console.log(str[9]);
