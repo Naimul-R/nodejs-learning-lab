a1e5d3de49313c47a5baecac6ae9c25f6d301166
Naimul-R <naimul.rabby08@gmail.com>
2026-03-17 17:17:28 +0600

    To learn string Methods is - str.toUpperCase().

---
+++ Loops-and-Strings/str_methods.js
@@ -0,0 +1,7 @@
+// String Methods --> These are build-in functions to manipulate a string
+// Note: Strings are immutable (not changable) in JS
+
+// str.toUpperCase() -->
+let str = "ILoveYou!"
+feelings = str.toUpperCase();
+console.log(feelings)
