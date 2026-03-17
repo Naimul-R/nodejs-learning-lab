89faaac7f01ba3be89c06ebc126731670ab2ec9a
Naimul-R <naimul.rabby08@gmail.com>
2026-03-17 16:39:05 +0600

    Learn strings and template literals string and how to express with literal string.

--- Loops-and-Strings/strings.js
+++ Loops-and-Strings/strings.js
@@ -9,3 +9,23 @@ str.length
 
 // String indicate ---> str/strName[position]
 console.log(str[9]);
+
+// Template Literals in JS --> A way to embedded expression in stings
+let specialSting = `This is a Template literals`;
+console.log(specialSting);
+
+let obj = {
+    item : "pen",
+    price : 10,
+};
+
+// String interpolation ${expression}
+let output = `The cost of ${obj.item} is ${obj.price} BDT`;
+console.log(output);
+
+// Template expression
+let outputt = `This is a template literal ${10+40}`;
+console.log(outputt)
+
+// (\n) use for next line
+// (\t) use for tab space
