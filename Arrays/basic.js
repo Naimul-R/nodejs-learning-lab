a29ccc8bd821e377871b573de68360222bf5a795
Naimul-R <naimul.rabby08@gmail.com>
2026-03-18 23:52:09 +0600

    study basic of arrays

--- Arrays/basic.js
+++ Arrays/basic.js
@@ -1,3 +1,8 @@
 // Array --> collection of items 
 
 // Create Array 
+let heros = ["ironman", "Hulk", "Spiderman", "Thor", "Batman"];
+let marks = [56, 89, 95, 34, 50];
+
+console.log(marks.length); // Property
+console.log(heros, marks);
