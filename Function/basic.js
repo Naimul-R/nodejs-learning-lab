a1e062a4ecd377998af499c86fb4233eaaffc5ac
Naimul-R <naimul.rabby08@gmail.com>
2026-03-24 19:16:37 +0600

    study function with parameters and function call.

--- Function/basic.js
+++ Function/basic.js
@@ -1,1 +1,38 @@
 // Block of code that perform a specific task, can be invoked whenever needed.
+
+function myFunction () {
+    console.log("Welcome to my JS learning lab.");
+    console.log("Cover all topics in this repo.");
+}
+
+// Function call or Invoked function
+myFunction();
+myFunction();
+
+// Function with parameters
+console.log("\nFunction with parameters...")
+function pmFunction (msg, n) { // msg --> parameter
+    // parameter --> input
+    console.log(msg, n);
+}
+
+pmFunction("I love JS!", 100) // Argument
+
+// Create Function --> 2 numbers, sum
+console.log("\nFunction user for sum 2 numbers...")
+function sumNum (num1, num2) {
+    console.log("20 + 30 = ", num1 + num2);
+}
+
+sumNum(30, 20);
+
+// Another way to return output
+// fun parameters --> like a local variable of function 
+function sum (x, y) {
+    // Local variable --> scope
+    s = x + y;
+    return s;
+}
+
+let val = sum(10, 20);
+console.log(val);
