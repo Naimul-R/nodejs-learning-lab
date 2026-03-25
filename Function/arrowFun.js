1ba22160d3229e8ccbeefda6f8498838dca2a6eb
Naimul-R <naimul.rabby08@gmail.com>
2026-03-25 13:12:19 +0600

    study arrow function

---
+++ Function/arrowFun.js
@@ -0,0 +1,24 @@
+// Arrow Function --> Compact way of writing a function. (=>)
+
+// Sum function
+function sum(a, b) {
+    return a+b;
+}
+
+const arrowSum = (a, b) => {
+    return a+b;
+}
+
+// Multiplication Function 
+function mul(x, y) {
+    console.log(x*y);
+}
+
+const arrowMul = (x, y) => {
+    return x*y;
+}
+
+
+const helloW = () => {
+    console.log("Hello world");
+}
