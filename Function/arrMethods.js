b93b7a371777e073424817850feef18cfcce4291
Naimul-R <naimul.rabby08@gmail.com>
2026-03-27 19:25:11 +0600

    study map method in function

--- Function/arrMethods.js
+++ Function/arrMethods.js
@@ -3,3 +3,16 @@
 // Map 
 // Creates a new array with the results of some operation. The value its callback returns are used to form new array.
 // Example : arr.map(callbackFnx(value, index, array))
+let num = [2, 4, 6, 8, 10];
+
+num.map((val) => {
+    console.log(val)
+})
+
+// return value 
+let newArr = num.map((val) => {
+    return val * 2;
+})
+console.log(newArr);
+
+// filter method
