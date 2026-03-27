d35c980f41a5e29db1405709f927498b3665e057
Naimul-R <naimul.rabby08@gmail.com>
2026-03-27 16:19:41 +0600

    Start to learn array methods

---
+++ Function/arrMethods.js
@@ -0,0 +1,5 @@
+// Some more array methods
+
+// Map 
+// Creates a new array with the results of some operation. The value its callback returns are used to form new array.
+// Example : arr.map(callbackFnx(value, index, array))
