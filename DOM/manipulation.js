4d0f8019caabdab885582e7f6828d9a1a121988b
Naimul-R <naimul.rabby08@gmail.com>
2026-04-08 16:53:24 +0600

    study DOM manipulation with selecting with class.

--- DOM/manipulation.js
+++ DOM/manipulation.js
@@ -5,3 +5,8 @@ let heading = document.getElementById("header");
 console.dir(heading)
 
 // Selecting with class -->
+let headings = document.getElementsByClassName("heading2-class");
+let para = document.getElementsByClassName("para-class")
+console.log(para)
+console.dir(headings);
+console.log(headings);
