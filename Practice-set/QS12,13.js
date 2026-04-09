44a49e3aa364fdc0dda9eb306e4bef10bb88f078
Naimul-R <naimul.rabby08@gmail.com>
2026-04-09 17:35:55 +0600

    DOM manipulaiton is now done to study with lecture.

--- Practice-set/QS12,13.js
+++ Practice-set/QS12,13.js
@@ -6,5 +6,15 @@ console.log(h2.innerText);
 h2.innerText = h2.innerText + " From Apna Collage student."; // Concatenate
 
 // Qs 13 
+// The Noob way
 let divs = document.querySelectorAll(".box");
-console.log(divs);
+/*divs[0].innerText = "New unique value 1";
+divs[1].innerText = "New unique value 2";
+divs[2].innerText = "New unique value 3"; */
+
+// Another way using loop --> 
+let idx = 1;
+for (div of divs) {
+    div.innerText = `New unique value ${idx}`;
+    idx++;
+}
