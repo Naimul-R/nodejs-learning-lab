dbafede928cc0eea50eacae850ab3914b2aeb04a
Naimul-R <naimul.rabby08@gmail.com>
2026-05-12 12:50:28 +0600

    Now we add functionality to perform action between user and computer.

--- Projects/Rock-Paper-Scissors/app.js
+++ Projects/Rock-Paper-Scissors/app.js
@@ -3,8 +3,17 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const ranIdx = Math.floor(Math.random() * 3);
+    return options[ranIdx];
+}
+
 const playGame = (userChoice) => {
     console.log("User choice = ", userChoice);
+    // Generate computer chocie
+    const compChoice = genCompChoice();
+    console.log("Computer choice = ", compChoice);
 }
 
 choices.forEach((choice) => {
