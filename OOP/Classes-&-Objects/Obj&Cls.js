e051fb2857e7652940a8454c1c87179759cadbb4
Naimul-R <naimul.rabby08@gmail.com>
2026-05-14 20:08:51 +0600

    create an simple object

---
+++ OOP/Classes-&-Objects/Obj&Cls.js
@@ -0,0 +1,10 @@
+// Creating an Objects --> 
+const student = {
+    fullName : "Naimul Rabby",
+    marks : 94.55,
+    printMarks : function () {
+        console.log("Marks = ", this.marks); //studnet.marks
+    },
+};
+
+// Now creating prototype --> 
