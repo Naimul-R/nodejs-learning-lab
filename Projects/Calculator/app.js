961184c4e81cefea996ba79691cfd4ae498a2629
Naimul-R <naimul.rabby08@gmail.com>
2026-05-23 17:13:38 +0600

    Now, finally calculator project is now for calculate

--- Projects/Calculator/app.js
+++ Projects/Calculator/app.js
@@ -7,11 +7,15 @@ let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if(e.target.innerHTML == '='){
-            string = eval(string);
+            string = String(eval(string));
             input.value = string;
         }
         else if (e.target.innerHTML == 'AC'){
             string = "";
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
             input.value = string;
         }
         else{
