1da871224c53dce8f2912526fffb9f2f300a17e3
Naimul-R <naimul.rabby08@gmail.com>
2026-06-12 23:56:18 +0600

    It's done to learn all of the topics

--- CPA/Async-Await.js
+++ CPA/Async-Await.js
@@ -25,7 +25,12 @@ function getData(dataId, getNextData) {
 
 // Async await 
 async function getAllData() {
+    console.log("Getting Data1....");
     await getData(1);
+    console.log("Getting Data2....");
     await getData(2);
+    console.log("Getting Data3....");
     await getData(3);
+    console.log("Getting Data4....");
+    await getData(4)
 }
