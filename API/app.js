25f2aa44338181e31e6d439d1e9a1be043e914ce
Naimul-R <naimul.rabby08@gmail.com>
2026-06-23 17:30:11 +0600

    Notes about API calls and how to fetch data using API

--- API/app.js
+++ API/app.js
@@ -2,11 +2,22 @@ const URL = "https://api.adviceslip.com/advice";
 const advicePara = document.querySelector("#advice");
 const btn = document.querySelector("#btn");
 
+// This even much more easier to write and read
 const getAdvice = async () => {
     let response = await fetch(URL);
     console.log(response);
     let data = await response.json();
     advicePara.innerText = data.slip.advice;
 }
+
+// Using promise chain
+// function getAdvice () {
+//     fetch(URL).then((response) => {
+//         return response.json();
+//     }).then((data) => {
+//         console.log(data);
+//         getAdvice.innerText = data.slip.advice;
+//     });
+// };
 
 btn.addEventListener("click", getAdvice);
