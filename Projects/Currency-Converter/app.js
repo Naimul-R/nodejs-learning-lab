a5436fe1f062a9688ee68dfe4ab2f04c2126ba5e
Naimul-R <naimul.rabby08@gmail.com>
2026-06-27 12:20:45 +0600

    Now we can get the exchange rate

--- Projects/Currency-Converter/app.js
+++ Projects/Currency-Converter/app.js
@@ -1,7 +1,10 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+// const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 
 for (let select of dropdowns) {
     for(currCode in countryList){
@@ -28,14 +31,20 @@ const updateFlag = (element) => {
     img.src = newSrc;
 };
 
-btn.addEventListener("click", (evt) =>{
+btn.addEventListener("click", async (evt) =>{
     evt.preventDefault();
     let amount = document.querySelector(".amount input");
     let amtval = amount.value;
     if (amtval === "" || amtval < 1) {
-        amount = 1;
+        amtval = 1;
         amount.value = "1";
     }
-
-    const URL = `${BASE_URL}/${fromCurr}/${toCurr}.json`;
-})
+    // console.log(fromCurr.value, toCurr.value);
+    // const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    // let rate = data[toCurr.value.toLowerCase()];
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    console.log(rate);
+});
