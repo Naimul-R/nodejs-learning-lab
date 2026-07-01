d331f220823b7025c86118e4d0c7deda7f3eb3f8
Naimul-R <naimul.rabby08@gmail.com>
2026-07-01 15:43:39 +0600

    add functionality to play and pasue button

--- Projects/Music-Player/app.js
+++ Projects/Music-Player/app.js
@@ -1,3 +1,17 @@
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcons = document.getElementById("ctrlIcons");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function PlayPause() {
+    if (ctrlIcons.classList.contains(fa-pause)){
+        song.pause();
+    }
+    else{
+        song.play();
+    }
+}
