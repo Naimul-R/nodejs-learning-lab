bc5a0bed6bec6c59ee0c9f9c5ca7114e0e190cca
Naimul-R <naimul.rabby08@gmail.com>
2026-07-01 16:07:25 +0600

    now this music player project is ready to pplay music

--- Projects/Music-Player/app.js
+++ Projects/Music-Player/app.js
@@ -8,10 +8,27 @@ song.onloadedmetadata = function(){
 }
 
 function PlayPause() {
-    if (ctrlIcons.classList.contains(fa-pause)){
+    if (ctrlIcons.classList.contains("fa-pause")){
         song.pause();
+        ctrlIcons.classList.remove("fa-pause");
+        ctrlIcons.classList.add("fa-play");
     }
     else{
         song.play();
+        ctrlIcons.classList.add("fa-pause");
+        ctrlIcons.classList.remove("fa-play");
     }
+}
+
+if (song.play()){
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+}
+
+progress.onchange = function() {
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcons.classList.add("fa-pause");
+    ctrlIcons.classList.remove("fa-play");
 }
